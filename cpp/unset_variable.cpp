b3290983744543bab0942d105590676df133465d
Alex Lindsay <adlinds3@ncsu.edu>
2015-11-03 19:25:06 -0500

    Cleaned up my programmin directory.

---
+++ cpp/unset_variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  float a;
+  float b = 1;
+  float c;
+  c = a*b;
+  std::cout << "undefined a is equal to " << a << std::endl;
+  std::cout << "c is equal to " << c << std::endl;
+  return 0;
+}
