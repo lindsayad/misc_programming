4e84d0387aa1f61445884caaa4b6cebd5e07153a
Alex Lindsay <alexlindsay239@gmail.com>
2018-07-25 16:14:28 -0600

    ambiguous-overloading program helped debug ambiguous overload in MooseADTypes

---
+++ cpp/ambiguous-overload.cpp
@@ -0,0 +1,35 @@
+#include "MooseADTypes.h"
+
+#include <iostream>
+#include <vector>
+
+template <typename T> using vec = std::vector<T>;
+
+template <typename T> vec<T> operator*(const T &scalar, const vec<T> &vector) {
+  std::cout << "called overload 1\n";
+  vec<T> return_vec;
+  for (const auto &element : vector)
+    return_vec.push_back(scalar * element);
+  return return_vec;
+}
+
+template <typename T> vec<T> operator*(const vec<T> &vector, const T &scalar) {
+  std::cout << "called overload 2\n";
+  vec<T> return_vec;
+  for (const auto &element : vector)
+    return_vec.push_back(scalar * element);
+  return return_vec;
+}
+
+int main() {
+  double x = 5;
+  std::vector<double> vec(10, 1);
+  operator*<double>(x *x, vec);
+  operator*<double>(vec, x);
+
+  VectorDN<Real> vector_dual_number;
+  VectorValue<Real> vector_value;
+  auto intermed = x * vector_value;
+
+  // operator*<Real>(x *vector_value, vector_dual_number);
+}
