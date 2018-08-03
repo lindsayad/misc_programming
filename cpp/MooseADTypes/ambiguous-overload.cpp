2b3050022f9710556692624e86211cfa4fa1d763
Alex Lindsay <alexlindsay239@gmail.com>
2018-08-02 19:47:55 -0600

    Newest commit in stride with lindsayad/moose/moose-ad-wip

--- cpp/MooseADTypes/ambiguous-overload.cpp
+++ cpp/MooseADTypes/ambiguous-overload.cpp
@@ -8,36 +8,38 @@ typedef VectorValue<Real> RealVectorValue;
 typedef TensorValue<Real> RealTensorValue;
 using MetaPhysicL::IsOperable;
 
-template <typename T> vec<T> operator*(const T &scalar, const vec<T> &vector) {
-  std::cout << "called overload 1\n";
-  vec<T> return_vec;
-  for (const auto &element : vector)
-    return_vec.push_back(scalar * element);
-  return return_vec;
-}
+// template <typename T> vec<T> operator*(const T &scalar, const vec<T> &vector)
+// {
+//   std::cout << "called overload 1\n";
+//   vec<T> return_vec;
+//   for (const auto &element : vector)
+//     return_vec.push_back(scalar * element);
+//   return return_vec;
+// }
 
-template <typename T> vec<T> operator*(const vec<T> &vector, const T &scalar) {
-  std::cout << "called overload 2\n";
-  vec<T> return_vec;
-  for (const auto &element : vector)
-    return_vec.push_back(scalar * element);
-  return return_vec;
-}
+// template <typename T> vec<T> operator*(const vec<T> &vector, const T &scalar)
+// {
+//   std::cout << "called overload 2\n";
+//   vec<T> return_vec;
+//   for (const auto &element : vector)
+//     return_vec.push_back(scalar * element);
+//   return return_vec;
+// }
 
 int main() {
-  double x = 5;
-  std::vector<double> vec(10, 1);
-  operator*<double>(x *x, vec);
-  operator*<double>(vec, x);
+  // double x = 5;
+  // std::vector<double> vec(10, 1);
+  // operator*<double>(x *x, vec);
+  // operator*<double>(vec, x);
 
-  VectorValueDN<Real> vector_dual_number;
-  VectorValue<Real> vector_value;
-  auto intermed = x * vector_value;
+  // VectorValueDN<Real> vector_dual_number;
+  // VectorValue<Real> vector_value;
+  // auto intermed = x * vector_value;
 
-  operator*<VectorValue, Real>(intermed, vector_dual_number);
-  operator*<VectorValue>(intermed, vector_dual_number);
-  operator*<>(intermed, vector_dual_number);
-  auto final = intermed * vector_dual_number;
+  // operator*<VectorValue, Real>(intermed, vector_dual_number);
+  // operator*<VectorValue>(intermed, vector_dual_number);
+  // operator*<>(intermed, vector_dual_number);
+  // auto final = intermed * vector_dual_number;
 
   ScalarDN<Real> scalar_ad_prop;
   VectorValueDN<Real> vector_ad_prop;
@@ -51,32 +53,32 @@ int main() {
   scalar_ad_prop = scalar_ad_prop * scalar_ad_prop;
 
   vector_ad_prop = scalar_ad_prop * RealVectorValue(1., 1., 1.);
-  scalar_ad_prop = vector_ad_prop * vector_ad_prop;
-  vector_ad_prop *= 2.;
-  vector_ad_prop = 2. * vector_ad_prop + vector_ad_prop * 2.;
+  // scalar_ad_prop = vector_ad_prop * vector_ad_prop;
+  // vector_ad_prop *= 2.;
+  // vector_ad_prop = 2. * vector_ad_prop + vector_ad_prop * 2.;
 
-  tensor_ad_prop =
-      scalar_ad_prop * RealTensorValue(1., 1., 1., 1., 1., 1., 1., 1., 1.);
+  // tensor_ad_prop =
+  //     scalar_ad_prop * RealTensorValue(1., 1., 1., 1., 1., 1., 1., 1., 1.);
 
-  vector_ad_prop = tensor_ad_prop * vector_ad_prop;
+  // vector_ad_prop = tensor_ad_prop * vector_ad_prop;
 
-  NumberArray<AD_MAX_DOFS_PER_ELEM, TensorValue<Real>> d_tensor{
-      RealTensorValue(1., 1., 1., 1., 1., 1., 1., 1., 1.)};
-  NumberArray<AD_MAX_DOFS_PER_ELEM, VectorValue<Real>> d_vector{
-      RealVectorValue(1., 1., 1.)};
-  auto tensor_result = tensor_reg_prop * d_tensor;
-  TypeTensor<Real> type_tensor_prop(tensor_reg_prop);
-  type_tensor_prop *= type_tensor_prop;
-  tensor_reg_prop *= tensor_reg_prop;
-  auto tensor_result2 = d_tensor * tensor_reg_prop;
+  // NumberArray<AD_MAX_DOFS_PER_ELEM, TensorValue<Real>> d_tensor{
+  //     RealTensorValue(1., 1., 1., 1., 1., 1., 1., 1., 1.)};
+  // NumberArray<AD_MAX_DOFS_PER_ELEM, VectorValue<Real>> d_vector{
+  //     RealVectorValue(1., 1., 1.)};
+  // auto tensor_result = tensor_reg_prop * d_tensor;
+  // TypeTensor<Real> type_tensor_prop(tensor_reg_prop);
+  // type_tensor_prop *= type_tensor_prop;
+  // tensor_reg_prop *= tensor_reg_prop;
+  // auto tensor_result2 = d_tensor * tensor_reg_prop;
 
-  tensor_ad_prop *= 2.;
-  tensor_ad_prop *= tensor_ad_prop;
-  tensor_ad_prop = tensor_ad_prop * tensor_ad_prop;
-  tensor_ad_prop = 2. * tensor_ad_prop + tensor_ad_prop * 2.;
-  tensor_ad_prop = 2. * tensor_ad_prop - tensor_ad_prop * 2.;
+  // tensor_ad_prop *= 2.;
+  // tensor_ad_prop *= tensor_ad_prop;
+  // tensor_ad_prop = tensor_ad_prop * tensor_ad_prop;
+  // tensor_ad_prop = 2. * tensor_ad_prop + tensor_ad_prop * 2.;
+  // tensor_ad_prop = 2. * tensor_ad_prop - tensor_ad_prop * 2.;
 
-  scalar_reg_prop = scalar_ad_prop;
-  vector_reg_prop = vector_ad_prop;
-  tensor_reg_prop = tensor_ad_prop;
+  // scalar_reg_prop = scalar_ad_prop;
+  // vector_reg_prop = vector_ad_prop;
+  // tensor_reg_prop = tensor_ad_prop;
 }
