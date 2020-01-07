3d0ba30a3ad628b9e6a0ad12b527a44a8bddf2cf
Alex Lindsay <alexlindsay239@gmail.com>
2020-01-07 12:02:52 -0800

    Demonstrate that one can have globally scoped and class scoped typedefs that work as expected with class scope typedef taking precedence in the class scope

--- cpp/class-scoped-typedef.cpp
+++ cpp/class-scoped-typedef.cpp
@@ -1,19 +1,73 @@
-class A
-{
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+
+class A {
 public:
   typedef double MyDouble;
 
   MyDouble foo();
 };
 
-A::MyDouble
-A::foo()
-{
-  return MyDouble(5);
+A::MyDouble A::foo() { return MyDouble(5); }
+
+template <typename> class BTempl;
+
+typedef BTempl<double> B;
+
+template <typename T> class BTempl {
+public:
+  typedef T type;
+  typedef BTempl<T> B;
+  BTempl() {}
+  BTempl(const B &) = default;
+  BTempl(B &&) = default;
+  B &operator=(const B &) = default;
+  B &operator=(B &&) = default;
+  ~BTempl() = default;
+
+  const B &foo();
+  const B &foo(const B &);
+};
+
+template <typename T> const typename BTempl<T>::B &BTempl<T>::foo() {
+  std::cout << std::is_same<B, BTempl<T>>::value << std::endl;
+  return *this;
 }
 
-int
-main()
-{
+template <typename T> const typename BTempl<T>::B &BTempl<T>::foo(const B &) {
+  std::cout << std::is_same<B, BTempl<T>>::value << std::endl;
+  return *this;
+}
+
+template <typename T> const BTempl<T> &foo(const BTempl<T> &b) {
+  std::cout << std::is_same<B, BTempl<T>>::value << std::endl;
+  return b;
+}
+
+template <typename T> class CTempl : public BTempl<T> {
+public:
+  CTempl() : B() {}
+};
+
+int main() {
   A().foo();
+  BTempl<double> b_double;
+  BTempl<int> b_int;
+  b_double.foo();
+  b_double.foo(b_double);
+
+  // compile error because we expect a type BTempl<double> as an argument
+  // b_double.foo(b_int);
+
+  foo(b_double);
+  b_int.foo();
+
+  // compile error because we expect a type BTempl<int> as an argument
+  // b_int.foo(b_double);
+
+  b_int.foo(b_int);
+  foo(b_int);
+
+  CTempl<double> c_double;
 }
